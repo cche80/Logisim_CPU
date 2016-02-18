956d0ad4fcbabd790cc999cdf5a1fd981eef881c
cche80 <chenchen.charliechen@gmail.com>
2016-02-18 14:49:57 -0800

    insert has been fixed; removeFirst done

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,33 +1,22 @@
 #include <iostream>
 #include <list>
+#include <queue>
 
 using namespace std;
 
 class Event {
 public:
-	Event* getNextEvent(void) {
-		return nextEvent;
-	}
+	Event* getNextEvent(void) { return nextEvent; }
 
-	Event* getPreEvent(void) {
-		return preEvent;
-	}
+	Event* getPreEvent(void) { return preEvent; }
 
-	bool getType(void) {
-		return type;
-	}
+	bool getType(void) { return type; }
 
-	void setType(bool inputType) {
-		type = inputType;
-	}
+	void setType(bool inputType) { type = inputType; }
 
-	double getEventTime(void) {
-		return eventTime;
-	}
+	double getEventTime(void) { return eventTime; }
 
-	void setEventTime(double inputTime) {
-		eventTime = inputTime;
-	}
+	void setEventTime(double inputTime) { eventTime = inputTime; }
 
 private:
 	Event* nextEvent;
@@ -39,24 +28,30 @@ private:
 class GEL {
 public:
 	GEL(void){}
-	GEL(Event inputEvent) {
+	GEL(Event inputEvent) {} //INCOMPLETE
 
+	void insert(Event &inputEvent) {
+		list<Event>::iterator itr;
+
+		for (itr = gel.begin(); itr != gel.end(); itr++) {
+			if (inputEvent.getEventTime() <= itr->getEventTime()) { break; }
+		}
+
+		gel.insert(itr, inputEvent); // this handles the case of empty gel
 	}
 
-	void insert(Event inputEvent) {
-		int counter = 0;
-
-		for (list<Event>::iterator itr = gel.begin(); itr != gel.end(); itr++) {
-			if (inputEvent.getEventTime() <= itr->getEventTime()) { break; }
-			counter++;
-		}
-		
-		gel.insert(inputEvent,counter); // it handles the case of empty gel
+	void removeFirst() { //remove first element
+		if (!gel.empty())
+			gel.pop_front();
 	}
 private:
-	list<Event> gel;
+	list<Event> gel; //Global Event List 
 };
 
+class Buffer { //implemented as FIFO queue
+public:
+private:
+};
 
 int main() {
 	return 0;
