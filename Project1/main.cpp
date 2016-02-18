145638527ec61100cfdf1f95f32f175066f6abb4
cche80 <chenchen.charliechen@gmail.com>
2016-02-17 22:25:46 -0800

    Event Object Proto

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+class Event {
+public:
+	Event* getNextEvent(void) {
+		return nextEvent;
+	}
+
+	Event* getPreEvent(void) {
+		return preEvent;
+	}
+
+	bool getType(void) {
+		return type;
+	}
+
+	void setType(bool inputType) {
+		type = inputType;
+	}
+
+private:
+	Event* nextEvent;
+	Event* preEvent;
+	bool type; // 1 for arrival event, 0 for departure event.
+};
+
 int main() {
 	cout << "testing" << endl;
 	cout << "push" << endl;
